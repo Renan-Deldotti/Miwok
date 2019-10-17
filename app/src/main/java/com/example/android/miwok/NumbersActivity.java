dd938750ab5e653156f1590aba698f6ffda3c407
Renan_Deldotti <renan.deldotti@hotmail.com>
2019-10-16 21:40:00 -0300

    Update final mod 2
    Final do modulo 2.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,29 +1,20 @@
 package com.example.android.miwok;
 
-import android.support.constraint.ConstraintLayout;
-import android.support.constraint.ConstraintSet;
-import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
 
         criaNumeros();
     }
@@ -44,23 +35,69 @@ public class NumbersActivity extends AppCompatActivity {
         listView.setAdapter(itemsAdapter);*/
 
         ArrayList<Word> words = new ArrayList<>();
-        words.add(new Word("One","Lutti"));
-        words.add(new Word("Two","Otiiko"));
-        words.add(new Word("Three","Tolookos"));
-        words.add(new Word("Four","Oyyisa"));
-        words.add(new Word("Five","Massokka"));
-        words.add(new Word("Six","Temmokka"));
-        words.add(new Word("Seven","Kenekaku"));
-        words.add(new Word("Eight","Kawinta"));
-        words.add(new Word("Nine","Wo'e"));
-        words.add(new Word("Ten","Na'aacha"));
+        words.add(new Word("One", "Lutti"));
+        words.add(new Word("Two", "Otiiko"));
+        words.add(new Word("Three", "Tolookos"));
+        words.add(new Word("Four", "Oyyisa"));
+        words.add(new Word("Five", "Massokka"));
+        words.add(new Word("Six", "Temmokka"));
+        words.add(new Word("Seven", "Kenekaku"));
+        words.add(new Word("Eight", "Kawinta"));
+        words.add(new Word("Nine", "Wo'e"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
+        words.add(new Word("Ten", "Na'aacha"));
 
-        WordAdapter adapter = new WordAdapter(this,words);
+        WordAdapter adapter = new WordAdapter(this, words);
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(adapter);
     }
+
     public void showIdToast(View view) {
         String textToShow = "Textview pressed: " + view.getId() + "\nThe Text is: " + ((TextView) findViewById(view.getId())).getText();
-        Toast.makeText(this,textToShow,Toast.LENGTH_LONG).show();
+        Toast.makeText(this, textToShow, Toast.LENGTH_LONG).show();
     }
 }
